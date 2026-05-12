fe9954b461ed8b8205a1cd4fe7ae56a6a90e7be1
ximencuisu <ximencuisu@users.noreply.github.com>
2026-05-12 15:44:43 +0800

    fix: QQ浏览器不识别type=module，添加@vitejs/plugin-legacy生成非模块回退

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   base: '/',
+  plugins: [
+    legacy({
+      targets: ['> 0.5%', 'not dead'],
+    }),
+  ],
   build: {
     outDir: 'dist',
   },
